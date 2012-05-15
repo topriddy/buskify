79a56e077349bed0bd68a2a7b0dc7b5df4d6fdb5
Ridwan Temitope Faro <topriddy@gmail.com>
2012-05-15 15:58:18 +0100

    implement code for reinitializing system, impl in Admin/Settings View

---
+++ src/com/buskify/util/Util.java
@@ -0,0 +1,55 @@
+package com.buskify.util;
+
+import java.util.List;
+
+import lombok.extern.log4j.Log4j;
+
+import com.buskify.dao.ProjectDao;
+import com.buskify.dao.StudentDao;
+import com.buskify.dao.SupervisorDao;
+import com.buskify.entity.Student;
+
+@Log4j
+public class Util {
+	
+	public static void resetAndInitializeSystemWithDB(){
+		resetAllocation();
+		resetSystem();
+		initializeSystemWithDB();
+	}
+
+	public static void initializeSystemWithDB(){
+		DataInitialiser.loadSupervisorsToDBVariant();
+		DataInitialiser.loadStudentsToDB();
+		DataInitialiser.loadProjectsToDB();
+	}
+	
+	public static void resetSystem() {
+		log.debug("Resetting System");
+		log.debug("Deleting All Data....");
+		
+		log.debug("Deleting all Projects");
+		new ProjectDao().deleteAll();
+		log.debug("Done, deleting all Projects");
+		
+		log.debug("Deleting all Students...");
+		new StudentDao().deleteAllExemptDefaultStudent();
+		log.debug("Done, deleting all Students");
+		
+		log.debug("Deleting all Supervisors...");
+		new SupervisorDao().deleteAllExemptDefaultSupervisor();
+		log.debug("Done, deleting all Supervisors");
+	}
+
+	public static void resetAllocation() {
+		log.debug("Resetting Allocation");
+		StudentDao studentDao = new StudentDao();
+		List<Student> studentList = studentDao.findAll();
+		for(Student student: studentList){
+			student.setAssignedProject(null);
+			studentDao.save(student);
+		}
+		
+		log.debug("Reset Allocation Successfully");
+	}
+}
