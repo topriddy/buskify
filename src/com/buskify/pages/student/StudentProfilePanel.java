c4ad1edcf6fe534a27221fd9cb34ce5c67b77599
Ridwan Temitope Faro <topriddy@gmail.com>
2012-05-15 04:19:52 +0100

    impl switching of pages/panels for student login page

---
+++ src/com/buskify/pages/student/StudentProfilePanel.java
@@ -0,0 +1,9 @@
+package com.buskify.pages.student;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class StudentProfilePanel extends Panel {
+	public StudentProfilePanel(String id){
+		super(id);
+	}
+}
